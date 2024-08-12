b27b0250f5b8df7f5925e53186910e5624aa7cae
Joakim <villokodehode@gmail.com>
2024-08-12 14:57:34 +0200

    *

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 export const AboutPage = ()=> {
+
+  const url = "https://api.adviceslip.com/advice"
+
+  const [advice, setAdvice] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+        const json = await response.json();
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(advice);
+  }, [advice]);
     return (
         <main className="min-h-screen flex justify-center items-center">
         <h1 className="text-3xl">this is the about page</h1>
+        <h2>{advice}</h2>
       </main>
     )
 }
--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+
 export const IndexPage = () => {
-    return (
-        <main className="min-h-screen flex justify-center items-center">
-        <h1 className="text-3xl">this is the index page</h1>
-      </main>
-    )
-}
+  const limit = 200;
+  const url = `https://eldenring.fanapis.com/api/bosses?limit=${limit}`;
+
+  const [bossData, setBossData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+        const json = await response.json();
+        setBossData(json.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(bossData);
+  }, [bossData]);
+
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl">this is the index page</h1>
+      <div id="bossCards">
+        {bossData.map((boss) => (
+          <div key={boss.id} className="boss-card">
+            <div className="boss-image">
+              <img
+                src={
+                  boss.image
+                    ? boss.image
+                    : "https://user-images.githubusercontent.com/88409867/128100295-6da3345d-e5b3-472f-9d37-9b849c21f810.png"
+                }
+                alt={boss.name}
+              />
+            </div>
+            <h2>{boss.name}</h2>
+            <p><strong>Region:</strong> {boss.region}</p>
+            <p><strong>Description:</strong> {boss.description}</p>
+            <p><strong>location:</strong> {boss.location}</p>
+            {/* <p><strong>:</strong> {boss.region}</p> */}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
