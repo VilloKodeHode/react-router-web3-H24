82a5794299ff1581db3eeed6aee27feb9734b420
Joakim <villokodehode@gmail.com>
2024-08-12 14:07:03 +0200

    changed a tag in header to Link component

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import { MENU_LIST } from "../../data/menu_list";
 
 export const Header = () => {
   return (
     <header className="h-[80px] bg-slate-300 w-full border-white border text-black">
         {MENU_LIST.map((item)=> (
-            <a href={item.href}>{item.text}</a>
+            <Link to={item.href}>{item.text}</Link>
         )) }
              {/* <a href="/">Home</a>
              <a href="/about">About</a>
